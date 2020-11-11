b838273cb429b2c8022b2b35cd1e4cee8aa3dd43
manuelsavino <manuelsavino@gmail.com>
2020-11-11 10:02:47 -0500

    form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -23,9 +23,10 @@ const Contact = () => (
 
         <form
           className="w-full md:w-1/2 md:ml-5"
+          method="post"
+          netlify-honeypot="bot-field"
           data-netlify="true"
           name="contact"
-          method="POST"
         >
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
